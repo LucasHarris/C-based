00e21c16dafe91976c930aaf66c9bb5c33f04bd3
Lucas Harris <67798594+LucasHarris@users.noreply.github.com>
2021-10-05 14:13:53 -0400

    Update Reverse-words.cpp

--- Reverse-words.cpp
+++ Reverse-words.cpp
@@ -12,18 +12,18 @@ Task:
 // Rule #1 of C++: Never declare namespace.
 
 int main() {
-/*[d---]*/  int x; // ------------------------------------------------------------ < Number of strings to be reversed >
-/*[o---]*/  std::cout<<"<?> Choose a number of words:"<<std::endl<<"<#> "; //----- < Prompt the user with the question >
-/*[i---]*/  std::cin>>x; // ------------------------------------------------------ < The number request >
-/*[d---]*/  std::string y; // ---------------------------------------------------- < The string to be entered >
-/*[a---]*/  for(int i=0;i<x;i++){
-/*[ao--]*/    std::cout<<std::endl<<"<"<<i<<"> ";
-/*[ai--]*/    std::cin>>y;
-/*[ad--]*/    std::string z;
-/*[aa--]*/    for(int j=y.length()-1;j>=0;j-=1){
-/*[ac--]*/       z+=y.at(j);
+/*[d---]*/  int x; // ------------------------------------------------------------ < Declaring the number for strings to be reversed  >
+/*[o---]*/  std::cout<<"<?> Choose a number of words:"<<std::endl<<"<#> "; //----- < Prompt the user with the question                >
+/*[i---]*/  std::cin>>x; // ------------------------------------------------------ < The number request                               >
+/*[d---]*/  std::string y; // ---------------------------------------------------- < Declaring the strings for what is entered        >
+/*[a---]*/  for(int i=0;i<x;i++){ // --------------------------------------------- < Looping for every string                         > 
+/*[ao--]*/    std::cout<<std::endl<<"<"<<i+1<<"> "; // --------------------------- < Printing out string number                       >
+/*[ai--]*/    std::cin>>y; // ---------------------------------------------------- < The String request                               >
+/*[ad--]*/    std::string z; // -------------------------------------------------- < Declaring the string for reversed strings        >
+/*[aa--]*/    for(int j=y.length()-1;j>=0;j-=1){ // ------------------------------ < Looping starting from the last char of string    >
+/*[ac--]*/       z+=y.at(j); // -------------------------------------------------- < Adding the letters to z starting from end of y   >
 /*[aa##]*/    }
-/*[ao--]*/    std::cout<<std::endl<<"<r> "<<z;
+/*[ao--]*/    std::cout<<"<r> "<<z; // ------------------------------------------- < Printing the resulting reversed string           >
 /*[a###]*/   }
   return 0; // End Program
 }
