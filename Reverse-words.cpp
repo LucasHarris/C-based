ed787f43d30c605201cfb9b2ce8322e4d0681520
Lucas Harris <67798594+LucasHarris@users.noreply.github.com>
2021-10-05 11:12:03 -0400

    Create Reverse-words.cpp

---
+++ Reverse-words.cpp
@@ -0,0 +1,14 @@
+/* { Date - Tuesday - October 5th, 2021 }
+Task:
+  - Take user input for an integer
+  - For the number given, request string type input
+  - For every string, print out its reverse
+  - End the program
+*/
+#include <iostream>
+#include <stdio.h>
+int main() {
+int x; // ----------------- < Number of strings to be reversed >
+std::cout<<"<?> Choose a number of words:"<<std::endl<<"<#> ";
+std::cin>>x; // ----------- 
+}
